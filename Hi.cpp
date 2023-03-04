686fa664413f26a35054d47c741668420eeac20a
Hoàng Trần <81359208+hoangtnedu@users.noreply.github.com>
2023-03-04 20:00:28 +0700

    Add files via upload

---
+++ Hi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 8;
+    int y = 6;
+    int sum = x + y;
+    int a;
+    // cout << "Nhập a:=";
+    // cin >> a;
+    // cout << " a= " << a;
+    cout << "Tổng =" << sum;
+
+    return 0;
+}
